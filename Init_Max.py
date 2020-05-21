8f4ec69c4d7908012b15a31eab37b852a991aa27
Stephan Chang <33191624+ChangShuaibin@users.noreply.github.com>
2020-05-20 20:45:37 -0400

    Add files via upload

--- Init_Max.py
+++ Init_Max.py
@@ -2,98 +2,86 @@
 
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
-import speech_recognition as sr
 import threading
 import queue
 import time
 from Object_detection import Object_detect
+import requests
+from picamera import PiCamera
+from motor import motor
 
-queue1 = queue.Queue()
-queue2 = queue.Queue()
-queue3 = queue.Queue()
-queue4 = queue.Queue()
-queue0 = queue.Queue()
-
-r = sr.Recognizer()
-sr.Microphone.list_microphone_names()
-r.energy_threshold = 400
-r.dynamic_energy_threshold = 1
-r.dynamic_energy_ratio = 1.8
-def listen():
-        command=''
-        while not 'exit' in command:
-            with sr.Microphone() as source:
-                print('Max is listening')
-                try:
-                    audio = r.listen(source, timeout=5)
-                    try:
-                        key_word = r.recognize_google(audio)
-                        if 'Max' in key_word :
-                            print('Yes sir')
-                            command=Give_command(command)
-                    except:
-                        pass
-                except sr.WaitTimeoutError:
-                    pass
-
-def Give_command(command):
-        with sr.Microphone() as source:
-            try:
-                # for testing purposes, we're just using the default API key
-                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-                # instead of `r.recognize_google(audio)`
-                audio = r.listen(source)
-                command = r.recognize_google(audio)
-                queue0.put(command)
-            except sr.UnknownValueError:
-                pass
-                # print("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
-            return(command)
+camera=PiCamera()
+m=motor()
+motor_queue = queue.Queue()
+camera_queue = queue.Queue()
+radar_queue = queue.Queue()
 
 
-def dequeue():
-        item=''
-        while 'exit' not in item:
-            item=queue0.get()
-            if 'turn left' in item:
+def retrive_command():
+    global command
+    command=''
+    old_index=''
+    while 'exit' not in command:
+        r=requests.get('http://192.168.99.164/')
+        text = r.text.split('<h1>')
+        text = text[1]
+        text = text.split('</h1>')
+        index=text[1]
+        command = text[0]
+        index=index.split('<h2>')
+        index=index[1]
+        index=index.split('</h2>')
+        index=index[0]
+        if old_index!=index:
+            parse_command()
+            old_index=index
+        time.sleep(2)
+
+
+def parse_command():
+            if 'turn left' in command:
                 queue1.put('turn left')
-            elif 'turn right' in item:
+            elif 'turn right' in command:
                 queue2.put('turn right')
-            elif 'can you see' in item:
+            elif 'can you see' in command:
                 queue3.put('can you see')
-            elif 'exit' in item:
+            elif 'exit' in command:
                 print('Good bye!')
                 queue1.put('exit')
                 queue2.put('exit')
                 queue3.put('exit')
                 queue4.put('exit')
             else:
-                queue4.put(item)
+                queue4.put(command)
 
 def turn_left():
         item=''
         while 'exit' not in item:
             item=queue1.get()
             if item == 'turn left':
-                print(threading.current_thread(),'Max is turning left')
+                print('Max is turning left')
+                m.angle2speed(90)
 
 def turn_right():
         item=''
         while 'exit' not in item:
             item=queue2.get()
             if item == 'turn right':
-                print(threading.current_thread(),'Max is turning right')
+                print('Max is turning right')
+                m.angle2speed(-90)
 
 def can_you_see():
         item=''
         while 'exit' not in item:
             item=queue3.get()
             if item == 'can you see':
-                print(threading.current_thread())
+                print('taking pictures and recognizing...')
+                camera.capture('/home/pi/Desktop/image.jpg')
                 app1=Object_detect()
-                app1.obj_detect('fruit.jpg')
+                app1.obj_detect('/home/pi/Desktop/image.jpg')
+                
+def forward():
+    pass
 
 
 def other():
@@ -101,25 +89,25 @@ def other():
         while 'exit' not in item:
             item=queue4.get()
             if 'exit' not in item:
-                print(threading.current_thread(),item)
+                print(item)
 
 
 if __name__=="__main__":
-
+    #retrive_thread=threading.Thread(target=retrive_command, name='retrive')
     left_thread = threading.Thread(target=turn_left,name='turn left')
     right_thread = threading.Thread(target=turn_right, name='turn right')
     see_thread = threading.Thread(target=can_you_see, name='see')
     other_thread = threading.Thread(target=other, name='other')
-    thread_dequeue=threading.Thread(target=dequeue, name='dequeue')
+
     left_thread.start()
     right_thread.start()
     see_thread.start()
     other_thread.start()
-    thread_dequeue.start()
-    listen()
+    retrive_command()
+    #retrive_thread.start()
     left_thread.join()
     right_thread.join()
     see_thread.join()
     other_thread.join()
-    thread_dequeue.join()
+    #retrive_thread.join()
 
