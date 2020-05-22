661e796007d7db6c008f3e33f1cb8c3faf8604cf
Stephan Chang <33191624+ChangShuaibin@users.noreply.github.com>
2020-05-21 22:06:08 -0400

    Update Max_commander_IOS.py

--- Max_commander_IOS.py
+++ Max_commander_IOS.py
@@ -1,34 +1,35 @@
 import speech
 import time
 import sound
-import dialogs
 import threading
 from flask import Flask, render_template
 import logging
+import console
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
-
+console.set_idle_timer_disabled(1)
 app = Flask(__name__)
 command=''
 index=0
-motor_key_words=['turn left', 'turn right', 'forward','backward','stop', 'start']
-camera_key_words=['can you see', 'picture','video']
+motor_key_words=['Turn left', 'Turn right', 'forward','backward','Stop', 'Start']
+camera_key_words=['Can you see', 'picture','video']
 
 def listen():
     global command
     global index
     recorder = sound.Recorder('speech.m4a')
-    while not 'exit' in command:
-        recorder.record()
-        while speech.is_speaking():
-            time.sleep(0.5)
-        recorder.stop()
-        try:
-            text = speech.recognize('speech.m4a')
-            [command, index]= stadardize_command(text[0][0], index)
-            print(command)
-        except:
-            pass
+    while 'exit' not in command:
+      speech.say('listening')
+      print('listening')
+      recorder.record()
+      time.sleep(3)
+      recorder.stop()
+      try:
+          text = speech.recognize('speech.m4a')
+          [command, index]= stadardize_command(text[0][0], index)
+          print(command)
+      except:
+          pass
 
 def stadardize_command(text, index):
     for item in motor_key_words:
@@ -37,7 +38,7 @@ def stadardize_command(text, index):
     for item in camera_key_words:
         if item in text:
             return ['camera '+ text,index+1]
-    if 'exit' in text:
+    if 'Exit' in text:
         return ['exit', index+1]
     return ['missed that', index]
 
@@ -54,5 +55,6 @@ def hello():
 if __name__=="__main__":
     thread_listen=threading.Thread(target=listen, name='listen')
     thread_listen.start()
-    #app.run(debug=True, port=80, host='0.0.0.0', use_reloader=False)
+    app.run(debug=True, port=80, host='0.0.0.0', use_reloader=False)
     thread_listen.join()
+    #listen()
